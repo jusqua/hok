68b03cab5143e241a57c55101aae9162769848be
Ádrian Gama <adrian.gama.se@gmail.com>
2026-06-27 20:28:20 -0300

    Add thresh kernel functor

--- include/hok.hpp
+++ include/hok.hpp
@@ -82,6 +82,11 @@ inline constexpr bool is_same_v =
 
 }  // namespace detail
 
+// TODO: Refact functors or derive from a common base
+// TODO: Deal with image data channels
+// TODO: Support unsampled image, if possible
+// TODO: Support nd_range
+
 template <
     typename input_type,
     typename output_type,
@@ -97,7 +102,6 @@ public:
     invert(input_type input_data, output_type output_data)
         : m_input_data(input_data), m_output_data(output_data) {}
 
-    // TODO: Support nd_range
     template <typename U = input_type, typename = std::enable_if_t<!detail::is_buffer<U>>>
     void operator()(const sycl::id<dimensions> id) const {
         m_output_data[id] = std::numeric_limits<value_type>::max() - m_input_data[id];
@@ -120,6 +124,49 @@ public:
 private:
     input_type m_input_data;
     output_type m_output_data;
+};
+
+template <
+    typename input_type,
+    typename output_type,
+    int dimensions = detail::element_dimensions<input_type>,
+    typename = std::enable_if_t<detail::is_numeric_v<input_type>>,
+    typename = std::enable_if_t<detail::is_numeric_v<output_type>>,
+    typename = std::enable_if_t<detail::is_same_v<input_type, output_type>>
+>
+class thresh {
+public:
+    using value_type = detail::element_t<input_type>;
+
+    thresh(input_type input_data, output_type output_data, value_type threshold,
+        value_type max_value = std::numeric_limits<value_type>::max(), value_type min_value = std::numeric_limits<value_type>::min())
+        : m_input_data(input_data), m_output_data(output_data), m_threshold(threshold), m_max_value(max_value), m_min_value(min_value) {}
+
+    template <typename U = input_type, typename = std::enable_if_t<!detail::is_buffer<U>>>
+    void operator()(const sycl::id<dimensions> id) const {
+        m_output_data[id] = m_input_data[id] > m_threshold ? m_max_value : m_min_value;
+    }
+
+    template <typename U = input_type, typename = std::enable_if_t<!detail::is_accessor<U>>>
+    sycl::event submit(const sycl::range<dimensions> range, sycl::queue& queue) {
+        if constexpr (detail::is_pointer<input_type>) {
+            return queue.parallel_for(range, (*this));
+        } else {
+            return queue.submit([=] (sycl::handler& handler) {
+                auto input_data = m_input_data.get_access(handler, sycl::read_only);
+                auto output_data = m_output_data.get_access(handler, sycl::write_only);
+
+                handler.parallel_for(range, hok::thresh(input_data, output_data, m_threshold, m_max_value, m_min_value));
+            });
+        }
+    }
+
+private:
+    input_type m_input_data;
+    output_type m_output_data;
+    value_type m_threshold;
+    value_type m_max_value;
+    value_type m_min_value;
 };
 
 }  // namespace hok
