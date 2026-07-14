d9986fbe9bebebf6691cf3158f1d892d249e58d2
Ádrian Gama <adrian.gama.se@gmail.com>
2026-07-14 14:21:56 -0300

    Kernels handles it's own data

--- include/hok.hpp
+++ include/hok.hpp
@@ -7,22 +7,13 @@
 
 namespace hok {
 
-namespace detail {
-
 template <int dimensions>
-class kernel {
+class kernel_impl {
 public:
-    kernel(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data)
-        : input_data(input_data), output_data(output_data), data_extent(data_extent) {}
-
     /// Must be implemented by derived classes
     void operator()(const sycl::item<dimensions> item) const;
 
 protected:
-    const sycl::range<dimensions> data_extent;
-    const float* input_data;
-    float* output_data;
-
     inline constexpr auto get_linear_id(const sycl::range<dimensions>& extent, const sycl::id<dimensions>& index) const {
         size_t id = 0;
         if constexpr (dimensions == 1) {
@@ -97,18 +88,6 @@ protected:
         }
         return result;
     }
-};
-
-template <int dimensions>
-class window_kernel : public kernel<dimensions> {
-public:
-    window_kernel(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data, const sycl::range<dimensions>& window_extent, const float* window_data)
-        : kernel<dimensions>(data_extent, input_data, output_data), window_data(window_data), window_extent(window_extent), window_halo(this->vec(window_extent / 2)) {}
-
-protected:
-    const sycl::range<dimensions> window_extent;
-    const sycl::vec<int, dimensions> window_halo;
-    const float* window_data;
 
     template <typename F>
     inline constexpr auto map(const sycl::range<dimensions>& range, const F&& apply) const {
@@ -136,115 +115,153 @@ protected:
     }
 };
 
-}  // namespace detail
-
 template <int dimensions>
-class invert : public detail::kernel<dimensions> {
+class invert : public kernel_impl<dimensions> {
 public:
     invert(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data)
-        : detail::kernel<dimensions>(data_extent, input_data, output_data) {}
+        : m_data_extent(data_extent), m_input_data(input_data), m_output_data(output_data) {}
 
     void operator()(const sycl::item<dimensions> item) const {
-        this->write(this->output_data, item, 1.0f - this->read(this->input_data, item));
-    }
-};
-
-template <int dimensions>
-class thresh : public detail::kernel<dimensions> {
-public:
-    thresh(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data, float threshold, float max_value = 1.0f, float min_value = 0.0f)
-        : detail::kernel<dimensions>(data_extent, input_data, output_data), threshold(threshold), max_value(max_value), min_value(min_value) {}
-
-    void operator()(const sycl::item<dimensions> item) const {
-        auto px = this->read(this->input_data, item);
-        px.x() = px.x() > this->threshold ? this->max_value : this->min_value;
-        px.y() = px.y() > this->threshold ? this->max_value : this->min_value;
-        px.z() = px.z() > this->threshold ? this->max_value : this->min_value;
-        this->write(this->output_data, item, px);
+        this->write(m_output_data, item, 1.0f - this->read(m_input_data, item));
     }
 
 private:
-    float threshold;
-    float max_value;
-    float min_value;
+    const sycl::range<dimensions> m_data_extent;
+    const float* m_input_data;
+    float* m_output_data;
 };
 
 template <int dimensions>
-class gray : public detail::kernel<dimensions> {
+class thresh : public kernel_impl<dimensions> {
 public:
-    gray(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data)
-        : detail::kernel<dimensions>(data_extent, input_data, output_data) {}
+    thresh(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data, float threshold, float max_value = 1.0f, float min_value = 0.0f)
+        : m_data_extent(data_extent), m_input_data(input_data), m_output_data(output_data), m_threshold(threshold), m_max_value(max_value), m_min_value(min_value) {}
 
     void operator()(const sycl::item<dimensions> item) const {
-        auto px = this->read(this->input_data, item);
+        auto px = this->read(m_input_data, item);
+        px.x() = px.x() > m_threshold ? m_max_value : m_min_value;
+        px.y() = px.y() > m_threshold ? m_max_value : m_min_value;
+        px.z() = px.z() > m_threshold ? m_max_value : m_min_value;
+        this->write(m_output_data, item, px);
+    }
+
+private:
+    const sycl::range<dimensions> m_data_extent;
+    const float* m_input_data;
+    float* m_output_data;
+    float m_threshold;
+    float m_max_value;
+    float m_min_value;
+};
+
+template <int dimensions>
+class gray : public kernel_impl<dimensions> {
+public:
+    gray(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data)
+        : m_data_extent(data_extent), m_input_data(input_data), m_output_data(output_data) {}
+
+    void operator()(const sycl::item<dimensions> item) const {
+        auto px = this->read(m_input_data, item);
         float gray = px.x() * 0.299f + px.y() * 0.587f + px.z() * 0.114f;
         px.x() = px.y() = px.z() = gray;
 
-        this->write(this->output_data, item, px);
+        this->write(m_output_data, item, px);
     }
+
+private:
+    const sycl::range<dimensions> m_data_extent;
+    const float* m_input_data;
+    float* m_output_data;
 };
 
 template <int dimensions>
-class convolve : public detail::window_kernel<dimensions> {
+class convolve : public kernel_impl<dimensions> {
 public:
     convolve(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data, const sycl::range<dimensions>& window_extent, const float* window_data)
-        : detail::window_kernel<dimensions>(data_extent, input_data, output_data, window_extent, window_data) {}
+        : m_data_extent(data_extent), m_input_data(input_data), m_output_data(output_data),
+          m_window_extent(window_extent), m_window_halo(this->vec(window_extent) / 2), m_window_data(window_data) {}
 
     void operator()(const sycl::item<dimensions> item) const {
         auto result = sycl::float4{0};
-        this->map(this->window_extent, [&](sycl::id<dimensions> fid) {
-            auto px = this->read(this->input_data, this->get_linear_id(item, this->vec(fid) - this->window_halo));
-            auto value = this->window_data[this->get_linear_id(this->window_extent, fid)];
+        this->map(m_window_extent, [&](sycl::id<dimensions> fid) {
+            auto px = this->read(m_input_data, this->get_linear_id(item, this->vec(fid) - m_window_halo));
+            auto value = m_window_data[this->get_linear_id(m_window_extent, fid)];
             result += px * value;
         });
-        this->write(this->output_data, item, result);
+        this->write(m_output_data, item, result);
     }
+
+private:
+    const sycl::range<dimensions> m_data_extent;
+    const sycl::range<dimensions> m_window_extent;
+    const sycl::vec<int, dimensions> m_window_halo;
+    const float* m_input_data;
+    const float* m_window_data;
+    float* m_output_data;
 };
 
 template <int dimensions>
-class erode : public detail::window_kernel<dimensions> {
+class erode : public kernel_impl<dimensions> {
 public:
     erode(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data, const sycl::range<dimensions>& window_extent, const float* window_data)
-        : detail::window_kernel<dimensions>(data_extent, input_data, output_data, window_extent, window_data) {}
+        : m_data_extent(data_extent), m_input_data(input_data), m_output_data(output_data),
+          m_window_extent(window_extent), m_window_halo(this->vec(window_extent) / 2), m_window_data(window_data) {}
 
     void operator()(const sycl::item<dimensions> item) const {
         auto result = sycl::float4(1.0f, 1.0f, 1.0f, 1.0f);
         auto result_sum = result.x() + result.y() + result.z();
 
-        this->map(this->window_extent, [&](sycl::id<dimensions> fid) {
-            auto px = this->read(this->input_data, this->get_linear_id(item, this->vec(fid) - this->window_halo));
-            auto value = this->window_data[this->get_linear_id(this->window_extent, fid)];
+        this->map(m_window_extent, [&](sycl::id<dimensions> fid) {
+            auto px = this->read(m_input_data, this->get_linear_id(item, this->vec(fid) - m_window_halo));
+            auto value = m_window_data[this->get_linear_id(m_window_extent, fid)];
             auto px_sum = px.x() + px.y() + px.z();
             if (value != 0.0f && result_sum > px_sum) {
                 result = px;
                 result_sum = px_sum;
             }
         });
-        this->write(this->output_data, item, result);
+        this->write(m_output_data, item, result);
     }
+
+private:
+    const sycl::range<dimensions> m_data_extent;
+    const sycl::range<dimensions> m_window_extent;
+    const sycl::vec<int, dimensions> m_window_halo;
+    const float* m_input_data;
+    const float* m_window_data;
+    float* m_output_data;
 };
 
 template <int dimensions>
-class dilate : public detail::window_kernel<dimensions> {
+class dilate : public kernel_impl<dimensions> {
 public:
     dilate(const sycl::range<dimensions>& data_extent, const float* input_data, float* output_data, const sycl::range<dimensions>& window_extent, const float* window_data)
-        : detail::window_kernel<dimensions>(data_extent, input_data, output_data, window_extent, window_data) {}
+        : m_data_extent(data_extent), m_input_data(input_data), m_output_data(output_data),
+          m_window_extent(window_extent), m_window_halo(this->vec(window_extent) / 2), m_window_data(window_data) {}
 
     void operator()(const sycl::item<dimensions> item) const {
         auto result = sycl::float4(0.0f, 0.0f, 0.0f, 0.0f);
         auto result_sum = result.x() + result.y() + result.z();
 
-        this->map(this->window_extent, [&](sycl::id<dimensions> fid) {
-            auto px = this->read(this->input_data, this->get_linear_id(item, this->vec(fid) - this->window_halo));
-            auto value = this->window_data[this->get_linear_id(this->window_extent, fid)];
+        this->map(m_window_extent, [&](sycl::id<dimensions> fid) {
+            auto px = this->read(m_input_data, this->get_linear_id(item, this->vec(fid) - m_window_halo));
+            auto value = m_window_data[this->get_linear_id(m_window_extent, fid)];
             auto px_sum = px.x() + px.y() + px.z();
             if (value != 0.0f && result_sum < px_sum) {
                 result = px;
                 result_sum = px_sum;
             }
         });
-        this->write(this->output_data, item, result);
+        this->write(m_output_data, item, result);
     }
+
+private:
+    const sycl::range<dimensions> m_data_extent;
+    const sycl::range<dimensions> m_window_extent;
+    const sycl::vec<int, dimensions> m_window_halo;
+    const float* m_input_data;
+    const float* m_window_data;
+    float* m_output_data;
 };
 
 }  // namespace hok
